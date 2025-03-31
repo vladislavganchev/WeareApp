2947d4dc3fc126723291f0446a1a55f4a9b78ab5
vladislavganchev <ganchev.vladislav@gmail.com>
2025-03-31 22:00:14 +0300

    signInPost locators

--- pages/SignInPage.ts
+++ pages/SignInPage.ts
@@ -2,10 +2,19 @@ import { Page, Locator } from '@playwright/test';
 import { BasePage } from './BasePage';
 
 export class SignInPage extends BasePage {
+    // Form locators
+    readonly usernameInput: Locator;
+    readonly passwordInput: Locator;
+    readonly loginButton: Locator;
+
     constructor(page: Page) {
         super(page);
-        // Add your locators here
+        // Form locators
+        this.usernameInput = this.page.locator('#username');
+        this.passwordInput = this.page.locator('#password');
+        this.loginButton = this.page.locator('button[value="Login"]');
     }
 
+    // Actions
     // Add your actions here
 } 
